2dc0ac135ba2b73c42aa4456d2a316a5782f76c3
rostikdiv <ramorostik@gmail.com>
2026-07-29 18:38:09 +0300

    fix(reservation): add Redis testcontainer for integration tests

--- src/test/java/com/parkflow/reservation/application/ReservationServiceRaceTest.java
+++ src/test/java/com/parkflow/reservation/application/ReservationServiceRaceTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.RabbitMQContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -38,6 +39,10 @@ class ReservationServiceRaceTest {
     @Container
     @ServiceConnection
     static final RabbitMQContainer rabbitmq = new RabbitMQContainer("rabbitmq:3-management-alpine");
+
+    @Container
+    @ServiceConnection(name = "redis")
+    static final GenericContainer<?> redis = new GenericContainer<>("redis:7-alpine").withExposedPorts(6379);
 
     @Autowired
     private ReservationService reservationService;
